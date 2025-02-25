d151c8f654fd15d7202a1d52e80cc74ca952db5a
cuyssi <cuyssi@hotmail.com>
2025-02-25 09:22:33 +0100

    fix functionality

--- src/main.js
+++ src/main.js
@@ -2,38 +2,62 @@ const URL_API = "http://localhost:3000/songs";
 const section = document.getElementById("section");
 
 async function getAllSongs() {
-  try {
-    const response = await fetch(`${URL_API}`);
-    const data = await response.json();    
-    return data;
-  } catch (error) {
-    console.log("Error al obtener las canciones:", error);
-  }
+    try {
+        const response = await fetch(`${URL_API}`);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.log("Error al obtener las canciones:", error);
+    }
+}
+
+async function createSong(newSong) {
+    try {
+        const response = await fetch(`${URL_API}`, {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(newSong)
+        })
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.log("The song could not be created:", error)
+    }
 }
 
 async function printAllSongs() {
-  const songs = await getAllSongs();
-  console.log(songs);
-  if (songs == "") {
-    return (section.innerHTML = `<p>Your library is empty.</p>
+    const songs = await getAllSongs();
+    console.log(songs);
+    section.innerHTML = "";
+
+    if (songs == "") {
+        return (section.innerHTML = `<p>Your library is empty.</p>
         <p>Start adding some songs!</p> `);
-  } else {
-    const article = document.createElement("article");
-    
-    songs.forEach((song) => {
-      section.appendChild(article);
-      article.innerHTML = `    
-            <img class="cover" src='${song.cover}'>
-            <div class="song-properties">
-            <p>${song.title}</p>
-            <p>${song.artist}</p>
-            <p>${song.genre}</p>      
-            <audio class="controls" src="${song.audio}" controls></audio>    
-            </div>
-            `;
-    });
-  }
+    } else {
+       
+        songs.forEach((song) => {
+            console.log("Canción recibida:", song); 
+            const article = document.createElement("article");            
+            article.innerHTML = `    
+                <img class="cover" src='${song.cover}'>
+                <div class="song-properties">
+                <p>${song.title}</p>
+                <p>${song.artist}</p>
+                <p>${song.genre}</p>      
+                <audio class="controls" src="${song.audio}" controls></audio>    
+                </div>     
+                `;
+                section.appendChild(article);
+        })       
+    }    
+}
+printAllSongs();
+
+async function openModal() {
+    try {
+
+    } catch (error) {
+        console.log("")
+    }
 }
 
-
-printAllSongs();
