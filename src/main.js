c92af769568a5ab448e6d3f6a0c3ba5f68f1cf2a
Aelnordev <loreriba99@gmail.com>
2025-02-28 16:08:58 +0100

    add delete pop up window

--- src/main.js
+++ src/main.js
@@ -144,7 +144,7 @@ async function printAllSongs() {
                     <button class="btnModify" onclick="getOneSong('${song.id}')"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
   <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z"/>
 </svg></button>
-                    <button  class="btnDelete" onclick="deleteSong('${song.id}')"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
+                    <button  class="btnDelete" onclick="openPopUp('${song.id}')"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
   <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5"/>
 </svg></button>
                 </div>
@@ -174,10 +174,11 @@ async function openModal(mode = "create", songData = null) {
 
         shadowModal.style.display = "block";
         modal.style.display = "block";
+        modal.innerHTML = "";
         form.innerHTML = `            
         <div class="modal-header">    
             <h4>${initialSentence}</h4> 
-            <button class="close-tag" onclick="close()"><i class="bi bi-x-lg"></i></button>
+            <button class="close-tag" onclick="closeModal()"><i class="bi bi-x-lg"></i></button>
         </div>   
             <input type="hidden" id="data-id" value= "${mode === "modify" && songData ?`${songData.id} ` : "" }" />
 
@@ -211,16 +212,47 @@ async function openModal(mode = "create", songData = null) {
             `;
         modal.appendChild(form);
     } catch (error) {
-        console.log("");
+        console.log("The modal could not be opened.");
     }
 }
 
-function close() {
+
+async function openPopUp(id) {
+try {
+    const modal = document.getElementById("modal");
+    document.body.style.overflow = "hidden";
+
+    shadowModal.style.display = "block";
+    modal.style.display = "block";
+    modal.innerHTML = "";
+    modal.innerHTML = `
+    <div class="pop-up">
+    <p> Are you sure you want to delete this song? </p>
+    <div class="popup-buttons-container">
+    <button class="pop-up-buttons" onclick="okAction('${id}')">OK</button>
+    <button class="pop-up-buttons" onclick="closeModal()">Cancel</button>
+    </div>
+    </div>
+    `
+} catch (error) {
+        console.log("The pop-up could not be opened.");
+}
+}
+
+
+function closeModal() {
     const modal = document.getElementById("modal");
     modal.style.display = "none";
     shadowModal.style.display = "none";
     document.body.style.overflow = "auto";
 }
+
+function okAction(id) {
+    deleteSong(id);
+    alert("You deleted the song.");
+    closeModal();
+}
+
 
 
 printAllSongs();
